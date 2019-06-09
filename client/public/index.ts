e2aad1f50ea026ba4b2650e55b6b38b1ef5c052a
Anthony Simmon <simmon.anthony@gmail.com>
2019-06-09 14:41:19 -0400

    Added pixi binding and images

--- client/public/index.ts
+++ client/public/index.ts
@@ -1,7 +1,35 @@
-import { IJoinRoomOpts } from '../../common/interfaces';
+/// <reference path="../../node_modules/pixi.js/pixi.js.d.ts" />
 
-const opts: IJoinRoomOpts = {
-  serverPort: 1
-};
+import { Application, Text, TextStyle } from 'pixi.js';
+import { Client } from 'colyseus.js';
+import { IGameState } from '../../common/interfaces';
 
-alert(JSON.stringify(opts));
+const client = new Client('ws://localhost:4321');
+const room = client.join('bomberman');
+
+const app = new Application({
+  width: 256,
+  height: 256,
+  antialias: true,
+  backgroundColor: 0xdddddd
+});
+
+const tilePixelSize = 32;
+
+room.onJoin.add(() => console.log(`successfully joined room ${room.id}`));
+room.onLeave.add(() => console.log(`leaved room ${room.id}`));
+room.onError.add((err: any) => console.log(`something wrong happened with room ${room.id}`, err));
+
+let initialized = false;
+room.onStateChange.add((state: IGameState) => {
+  if (!initialized) {
+    app.renderer.resize(state.width * tilePixelSize, state.height * tilePixelSize);
+    document.getElementById('pixi')!.appendChild(app.view);
+    initialized = true;
+  }
+
+  console.log(JSON.stringify(state));
+});
+
+client.onClose.add(() => console.log('connection has been closed'));
+client.onError.add((err: any) => console.log('something wrong happened with client: ', err));
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const copy = require('copy-webpack-plugin');
 
 module.exports = {
   entry: [path.resolve(__dirname, 'public/index.html'), path.resolve(__dirname, 'public/index.ts')],
@@ -22,5 +23,13 @@ module.exports = {
         use: 'ts-loader'
       }
     ]
-  }
+  },
+  plugins: [
+    new copy([
+      {
+        from: path.resolve(__dirname, 'public/assets'),
+        to: path.resolve(__dirname, 'public-dist/assets')
+      }
+    ])
+  ]
 };
