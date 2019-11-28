f41f35b02c0468e008ca73837b811c4ea0a78f0e
Anthony Simmon <anthony.simmon@gsoft.com>
2019-11-28 17:18:50 -0500

    Removed unused code

--- src/client/utils.ts
+++ src/client/utils.ts
@@ -2,7 +2,7 @@ import process from 'process';
 import path from 'path';
 import fs from 'fs';
 
-import { IGameState, IJoinRoomOpts, ISimpleGameState } from '../types';
+import { IJoinRoomOpts } from '../types';
 
 export function getJoinOptions(): IJoinRoomOpts {
   const configJsonFileName = 'config.json';
@@ -67,74 +67,6 @@ export async function sleepAsync(milliseconds: number): Promise<void> {
   return new Promise(resolve => setTimeout(resolve, milliseconds));
 }
 
-export function createSanitizedStateCopyForBot(state: IGameState): ISimpleGameState {
-  state = jsonClone(state);
-
-  const sgs: ISimpleGameState = {
-    tick: state.tick,
-    state: state.state,
-    tiles: state.tiles,
-    players: {},
-    bombs: {},
-    bonuses: {},
-    width: state.width,
-    height: state.height,
-    suddenDeathCountdown: state.suddenDeathCountdown,
-    suddenDeathEnabled: state.suddenDeathEnabled
-  };
-
-  for (const playerId in state.players) {
-    const p = state.players[playerId];
-
-    sgs.players[playerId] = {
-      x: p.x,
-      y: p.y,
-      id: p.id,
-      bombsLeft: p.bombsLeft,
-      maxBombs: p.maxBombs,
-      bombRange: p.bombRange,
-      alive: p.alive,
-      score: p.score,
-      color: p.color,
-      respawning: p.respawning
-    };
-  }
-
-  for (const bombId in state.bombs) {
-    const b = state.bombs[bombId];
-
-    if (b.countdown > 0) {
-      sgs.bombs[bombId] = {
-        x: b.x,
-        y: b.y,
-        playerId: b.playerId,
-        countdown: b.countdown,
-        range: b.range
-      };
-    }
-  }
-
-  for (const bonusId in state.bonuses) {
-    const b = state.bonuses[bonusId];
-
-    sgs.bonuses[bonusId] = {
-      x: b.x,
-      y: b.y,
-      type: b.type
-    };
-  }
-
-  return sgs;
-}
-
 export function jsonClone<T>(obj: T): T {
   return JSON.parse(JSON.stringify(obj));
-}
-
-export function shuffle<T>(a: T[]): T[] {
-  for (let i = a.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [a[i], a[j]] = [a[j], a[i]];
-  }
-  return a;
 }
